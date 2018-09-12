154da1df3bf695ecad72d90a691fc111541b92d3
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-09-12 18:16:23 -0500

    updated connnection.js

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,22 @@ require("dotenv").config();
 
 
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "1234",
   database: "burgers_db"
+
 });
+};
+
 
 // var connection = mysql.createConnection({
 //   host: "localhost",
